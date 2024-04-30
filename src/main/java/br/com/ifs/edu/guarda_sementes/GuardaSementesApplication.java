8b8e511cb78dda61c3ac619798cdae136b68dc02
vitorgsn <dev.vitor99@gmail.com>
2024-04-30 20:41:49 -0300

    first commit

---
+++ src/main/java/br/com/ifs/edu/guarda_sementes/GuardaSementesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifs.edu.guarda_sementes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardaSementesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardaSementesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifs/edu/guarda_sementes/GuardaSementesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifs.edu.guarda_sementes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardaSementesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
